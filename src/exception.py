e87298d9b1cb2d0e3a419d2b980f6ea66fb8bb84
pushpendra chauhan <pushpendrachauhan400@gmail.com>
2026-07-23 11:05:13 +0530

    data_ingestion

--- src/exception.py
+++ src/exception.py
@@ -18,9 +18,3 @@ class customException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise customException(e,sys)
